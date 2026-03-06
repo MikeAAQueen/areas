63b4b41c250bc06013f6445518624097d143cd16
Michael Queen <michael.queen@aluno.impacta.edu.br>
2026-03-06 09:33:27 -0300

    Inclusão do calculo da area de retangulos

--- vegeta.py
+++ vegeta.py
@@ -1,9 +1,17 @@
 def area_quadrado(lado):
     return lado * lado
 
+def area_retangulo(base, altura):
+    return base * altura
+
 def main():
     lado = float(input("Digite o lado do quadrado: "))
-    area = area_quadrado(lado)
-    print(f"A área do quadrado é: {area:.2f}")
+    area1 = area_quadrado(lado)
+    print(f"A área do quadrado é: {area1:.2f}")
+    
+    base = float(input("Digite a base do retângulo: "))
+    altura = float(input("Digite a altura do retângulo: "))
+    area2 = area_retangulo(base, altura)
+    print(f"A área do retângulo é: {area2:.2f}")
 
 main()
